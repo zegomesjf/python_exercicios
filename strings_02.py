d8951e2f561e13a0ef0d3472716ea0e80f87038c
zegomesjf <zegomesjf@gmail.com>
2009-03-31 10:20:46 -0300

    3 da serie de listas

---
+++ strings_02.py
@@ -0,0 +1,6 @@
+#.*.coding: latin1*.*
+#Nome ao contrário em maiúsculas
+
+name = raw_input('Digite seu nome: ')
+name = name.upper()
+print('Seu nome ao contrário em maiúsculas é: ') + name[::-1]
---
+++ strings_03.py
@@ -0,0 +1,7 @@
+#encoding: latin1
+#Nome na vertical
+
+name = raw_input('Digite seu nome: ')
+for i in range(len(name)):
+    print name[i:i+1] 
+    
