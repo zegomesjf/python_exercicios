e35113737d06b934b374943b471c1e18eca1cdf1
zegomesjf <zegomesjf@gmail.com>
2009-03-31 16:58:12 -0300

    mais um de estr. seq.

---
+++ estrutura_sequencial_06.py
@@ -0,0 +1,22 @@
+#encoding: latin1
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+import math
+class Circulo(object):
+    def __init__(self):
+        self.__raio = 0.00
+
+    def get_raio(self):
+        return self.__raio
+
+    def set_raio(self, valor):
+        if isinstance(valor, (float, int, long)):
+            self.__raio = valor
+        else:
+            raise TypeError ("Raio deve ser um valor numérico")
+    
+    def __str__(self):
+        pi = math.pi
+        return pi * pow(get_raio(self),2)
+
+    raio = property (get_raio, set_raio)
